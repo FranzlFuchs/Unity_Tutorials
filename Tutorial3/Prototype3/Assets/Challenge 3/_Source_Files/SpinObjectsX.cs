91150c84bd6f7d443d743d307137612b011e518d
FranzlFuchs <60236110+FranzlFuchs@users.noreply.github.com>
2022-03-17 14:04:57 +0100

    Challenge3 done

---
+++ Tutorial3/Prototype3/Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObjectsX : MonoBehaviour
+{
+    public float spinSpeed;
+
+    void Start(){
+
+        spinSpeed = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+    }
+}
